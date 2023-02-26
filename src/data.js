b7c99e23aa6b49b5b843f3cd232512ca6a395d28
ozzkan-burak <brk.ozzkan@gmail.com>
2023-02-27 00:05:11 +0300

    episode1

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const data = {
+  items: [
+    {
+      name: "Test Product-1",
+      price: "1200",
+      qty: "5"
+    },
+    {
+      name: "Test Product-2",
+      price: "200",
+      qty: "6"
+    },
+    {
+      name: "Test Product-3",
+      price: "3200",
+      qty: "2"
+    },
+    {
+      name: "Test Product-4",
+      price: "1250",
+      qty: "1"
+    },
+    {
+      name: "Test Product-5",
+      price: "1300",
+      qty: "3"
+    },
+  ]
+}
